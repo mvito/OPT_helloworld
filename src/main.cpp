a6bda3b1ace19c5ddba62e3b80c32b016fdf479e
mvito <mvito@github.com>
2017-04-21 15:55:48 +0100

    main update

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ void SolveWarping(int width, int height, float* unknown, float* target) {
     param.collectPerKernelTimingInfo = 1;
     Opt_State* state = Opt_NewState(param);
     // load the Opt DSL file containing the cost description
-    Opt_Problem* problem = Opt_ProblemDefine(state,"warpingEnergy.opt", NULL);
+    Opt_Problem* problem = Opt_ProblemDefine(state,"../src/laplacian.opt", "solverGaussNewtonGPU");
     // describe the dimensions of the instance of the problem
     uint32_t dims[] = { width, height };
     Opt_Plan* plan = Opt_ProblemPlan(state, problem, dims);
@@ -32,15 +32,19 @@ int main(){
   int width, height;
   float *unknown;
   float *target;
-  Mat image;
+  Mat image, output;
   image = imread("../data/cat512.png");
+  image.convertTo(image,CV_32F);
 
   namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
   imshow( "Display window", image );                   // Show our image inside it.
 
   width = image.cols;
   height = image.rows;
+  output = Mat::zeros(height,width,CV_32F);
 
+  target = (float*)image.data;
+  unknown = (float*)output.data;
   SolveWarping(width, height, unknown, target);
 
   waitKey(0);                                          // Wait for a keystroke in the window
